e97f34bc312bf9a5cbe7cb3450391c97cd4381b1
Diya-Dangol <diya.dangol15@gmail.com>
2021-01-17 11:24:45 +0545

    Updates

---
+++ precache-manifest.6a570f23876a2ef15c2fcbf4c8b50d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ecefeb791900094995e34f8e2d996f",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "b0df611d66fd206d9422",
+    "url": "/movie-app/static/css/main.06c5da00.chunk.css"
+  },
+  {
+    "revision": "847946b1ab9a74078f3e",
+    "url": "/movie-app/static/js/2.11edc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.11edc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0df611d66fd206d9422",
+    "url": "/movie-app/static/js/main.e5ec6c07.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
